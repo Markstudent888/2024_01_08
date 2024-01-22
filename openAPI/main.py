ebaf70690df89c0f6019997d030c5349d7e40704
Mark <studenttehcheng8@gmail.com>
2024-01-22 13:53:47 +0800

    main

--- openAPI/main.py
+++ openAPI/main.py
@@ -21,3 +21,31 @@ def counter(c:int):
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q:str | None = None):
     return {"item_id": item_id, "q": q}
+
+'''
+from typing import Union
+from fastapi import FastAPI
+import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    counter = redis_conn.incr('test:increment',1)
+    return {"Counter": counter}
+
+@app.get("/counter/{c}")
+def counter(c:int):
+    counter = redis_conn.incr('test:increment',c)
+    return {"Counter": counter}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q:str | None = None):
+    return {"item_id": item_id, "q": q}
+'''
