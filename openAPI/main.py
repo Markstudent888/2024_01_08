ac77acd08b753b8d842716d2aacc878431f39d06
Mark <studenttehcheng8@gmail.com>
2024-01-24 12:02:39 +0800

    main()

--- openAPI/main.py
+++ openAPI/main.py
@@ -1,54 +1,63 @@
-from typing import Union
-from fastapi import FastAPI
-import redis
-import os
-from dotenv import load_dotenv
-load_dotenv()
-redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+from machine import Pin, ADC
+from tools import connect,reconnect
+import time
+import urequests
 
-app = FastAPI()
+red_led = Pin(15,mode=Pin.OUT)
+btn = Pin(14,mode=Pin.PULL_DOWN)
+is_press = False
+connect()
 
-@app.get("/")
-def read_root():
-    counter = redis_conn.incr('test:increment',1)
-    return {"Counter": counter}
+def getCurrentTime():
+    times_tuple = time.localtime()
+    currentTime = f'{times_tuple[0]}-{times_tuple[1]}-{times_tuple[2]} {times_tuple[3]}:{times_tuple[4]}:{times_tuple[5]}'
+    return currentTime
 
-@app.get("/counter/{c}")
-def counter(c:int):
-    counter = redis_conn.incr('test:increment',c)
-    return {"Counter": counter}
+def getTemperature():
+    sensor_temp = machine.ADC(4)
+    conversion_factor = 3.3 / (65535)
+    reading = sensor_temp.read_u16() * conversion_factor
+    temperature = 27 - (reading - 0.706)/0.001721
+    return temperature
 
-@app.get("/pico_w/{date}")
-async def read_item(date:str ,address:str,celsius:float=0.0):
-    print(f"日期:{date}")
-    print(f"位置:{address}")
-    print(f"攝氏:{celsius}")
-    return {"狀態":"儲存成功"}
+def getLightValue():
+    adc_light = ADC(Pin(28))
+    light_value = adc_light.read_u16()
+    return light_value
 
-'''
-from typing import Union
-from fastapi import FastAPI
-import redis
-import os
-from dotenv import load_dotenv
-load_dotenv()
-redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    counter = redis_conn.incr('test:increment',1)
-    return {"Counter": counter}
-
-@app.get("/counter/{c}")
-def counter(c:int):
-    counter = redis_conn.incr('test:increment',c)
-    return {"Counter": counter}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q:str | None = None):
-    return {"item_id": item_id, "q": q}
-'''
+while True:
+    if btn.value():
+        #解決按下彈跳
+        time.sleep_ms(50)
+        if btn.value():
+            is_press = True
+            red_led.value(1)
+    else:
+        #解決放開彈跳
+        time.sleep_ms(50)
+        if btn.value() == False:
+            if is_press == True:     
+                print('release')                
+                currentTime = getCurrentTime()
+                temperature= getTemperature()
+                light=getLightValue()
+                
+                is_press = False
+                
+                url_str = f'https://openapi-test-slqg.onrender.com/pico_w/{currentTime}?address=tvdi_school&celsius={temperature}&light={light}'
+                try:
+                    response = urequests.get(url_str)
+                    pass
+                except:
+                    print("ap出現問題")            
+                    reconnect()
+                else:
+                    if response.status_code == 200:            
+                        print("傳送訊息成功")
+                    else:
+                        print("傳送失敗(server出現錯誤)")
+                    response.close()
+                
+            
+        
+            red_led.value(0)
