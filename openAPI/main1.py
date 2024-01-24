efb080cbb8187e57d1bd95d69b9803a087cd1024
Mark <studenttehcheng8@gmail.com>
2024-01-24 14:51:22 +0800

    modify_main1

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -5,6 +5,71 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+async def get_item(item_id:int):
+    print(f"使用者輸入了:{item_id}")
+    return {"item_id":item_id}
+
+@app.get("/items/{date}/{celsius}")
+async def get_item(date:str,celsius:float):
+    print(f"日期:{date}")
+    print(f"溫度:{celsius}")
+    return {"日期":date,"攝氏溫度":celsius}
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+
+
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float,light:float):
+    #print(f"日期:{date}")
+    redis_conn.rpush('pico_w:date',date)
+    #print(f"位置:{address}")
+    redis_conn.hset('pico_w:address',mapping={date:address})
+    #print(f"攝氏:{celsius}")
+    redis_conn.hset('pico_w:temperature',mapping={date:celsius})
+    #print(f"光線:{light}")
+    redis_conn.hset('pico_w:light',mapping={date:light})
+
+    date_get = redis_conn.lrange('pico_w:date',-1,-1)[0].decode()
+    address_get = redis_conn.hget('pico_w:address',date_get).decode()
+    temperature_get = redis_conn.hget('pico_w:temperature',date_get).decode()
+    light_get = redis_conn.hget('pico_w:light',date_get).decode()
+    print(date_get)
+    print(address_get)
+    print(temperature_get)
+    print(light_get)
+    
+    return {"狀態":"儲存成功"}
+
+@app.get("/pico_w/{date}")
+async def read_item(date:str ,address:str,celsius:float=0.0):
+    print(f"日期:{date}")
+    print(f"位置:{address}")
+    print(f"攝氏:{celsius}")
+    return {"狀態":"儲存成功"}
+'''
+from typing import Union
+from fastapi import FastAPI
+import redis
+import os
+from dotenv import load_dotenv
+load_dotenv()
+redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 app = FastAPI()
 
 
@@ -47,7 +112,10 @@ async def read_item(date:str ,address:str,celsius:float=0.0):
 
 
 
-'''
+---
+
+
+
 from typing import Union
 from fastapi import FastAPI
 import redis
@@ -62,4 +130,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+    -----
     '''
